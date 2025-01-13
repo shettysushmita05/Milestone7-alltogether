c83b8dd8d5fa30df0db1be3ea1255cd09c162c56
Sushmita Shetty <sushmitashetty8080@gmail.com>
2025-01-13 12:27:11 +0530

    added the changes for not found

--- task2/src/components/NotFound.jsx
+++ task2/src/components/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = () => {
   return (
-    <div>
+    <div className='not-found'>
       <h1>404 - Page Not Found</h1>
       <p>The page you are looking for does not exist.</p>
     </div>
